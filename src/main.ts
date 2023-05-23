7739080536694ed7ab6e4554a93465d9fa47566e
liuzhihaoa <liu413780791@qq.com>
2023-05-23 17:45:46 +0800

    引入router和element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
+/*
+ * @Author: liuzhihao
+ * @email: liuzhihao@hatech.com.cn
+ * @Date: 2023-05-22 16:17:16
+ * @LastEditors: liuzhihao
+ * @LastEditTime: 2023-05-23 13:43:27
+ * @description: 描述
+ */
 import { createApp } from 'vue';
-import './style.css';
+import router from '@/routers';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+import 'element-plus/dist/index.css';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import '@/styles/reset.scss';
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app.use(router).mount('#app');
