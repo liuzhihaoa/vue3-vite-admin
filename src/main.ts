eddedb54804283a4d52d3c05dd156149e9ba12a2
liuzhihaoa <liu413780791@qq.com>
2023-05-26 11:01:23 +0800

    引入暗黑模式样式

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
  * @email: liuzhihao@hatech.com.cn
  * @Date: 2023-05-22 16:17:16
  * @LastEditors: liuzhihao
- * @LastEditTime: 2023-05-25 14:30:16
+ * @LastEditTime: 2023-05-26 11:00:01
  * @description: 描述
  */
 import { createApp } from 'vue';
@@ -11,9 +11,16 @@ import router from '@/routers';
 import pinia from './store';
 import App from './App.vue';
 
+// element css
 import 'element-plus/dist/index.css';
-import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+// element dark css
+import 'element-plus/theme-chalk/dark/css-vars.css';
+// custom dark css
+import '@/styles/theme/element-dark.scss';
+// reset css
 import '@/styles/reset.scss';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
