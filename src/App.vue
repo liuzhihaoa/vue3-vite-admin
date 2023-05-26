d7d5d8013c34cd1992b2b47a146921ba4c461a87
liuzhihaoa <liu413780791@qq.com>
2023-05-26 11:19:49 +0800

    根组件初始化主题

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,15 @@
  * @email: liuzhihao@hatech.com.cn
  * @Date: 2023-05-23 17:41:39
  * @LastEditors: liuzhihao
- * @LastEditTime: 2023-05-23 17:42:09
+ * @LastEditTime: 2023-05-26 11:19:05
  * @description: 描述
 -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTheme } from '@/hook/useTheme';
+const { initTheme } = useTheme();
+// 初始化主题
+initTheme();
+</script>
 
 <template>
   <el-config-provider>
