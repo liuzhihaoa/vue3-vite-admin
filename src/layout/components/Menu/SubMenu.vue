619565cd5bb57464d50bf1fb6cd402ab70c4bbf6
liuzhihaoa <liu413780791@qq.com>
2023-06-06 15:42:28 +0800

    新增菜单组件

---
+++ src/layout/components/Menu/SubMenu.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Author: liuzhihao
+ * @email: liuzhihao@hatech.com.cn
+ * @Date: 2023-05-26 11:42:25
+ * @LastEditors: liuzhihao
+ * @LastEditTime: 2023-06-06 15:08:10
+ * @description: 描述
+-->
+<template>
+  <template v-for="subItem of menuList" :key="subItem.path">
+    <el-sub-menu v-if="subItem.children?.length > 0" :index="subItem.path">
+      <template #title>
+        <el-icon>
+          <component :is="subItem.meta.icon"></component>
+        </el-icon>
+        <span>{{ subItem.meta.title }}</span>
+      </template>
+      <SubMenu :menuList="subItem.children!" />
+    </el-sub-menu>
+    <el-menu-item v-else :index="subItem.path">
+      <el-icon>
+        <component :is="subItem.meta.icon"></component>
+      </el-icon>
+      <template #title>
+        <span>{{ subItem.meta.title }}</span>
+      </template>
+    </el-menu-item>
+  </template>
+</template>
+
+<script lang="ts" setup>
+defineProps<{ menuList: Menu.MenuOptions[] }>();
+</script>
+
+<style lang="scss"></style>
